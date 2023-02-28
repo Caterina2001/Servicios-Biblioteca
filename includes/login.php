24c2dfc05285872a0d5305cd1fcd0d473948c6ea
Caterina Giannecchini <rosariocaterina7@gmail.com>
2023-02-28 15:47:03 -0400

    Login actualizado y funcional

--- includes/login.php
+++ includes/login.php
@@ -1,21 +1,17 @@
+<html>
 
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
+    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link rel="stylesheet" type="text/css" href="login.css">
-    <link rel="stylesheet" href="bootstrap.min.css" />
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="../css/login.css">
+        <link rel="stylesheet" href="../css/bootstrap.min.css" />
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	
+    </head>
 
-</head>
-
-<body>
+    <body>
 
         
             <div class="container-fluid h-custom">
@@ -25,41 +21,36 @@
                     class="imagen">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                  <form>
+                  <form action="_functions.php" method="POST" >
           
                     <div class="divider d-flex align-items-center my-4">
-                      <p class="text-center fw-bold mx-3 mb-0">Iniciar Sesión</p>
+                      <p class="text-center fw-bold mx-3 mb-0 TColor">Iniciar Sesión</p>
                     </div>
           
-                    <!-- Email input -->
+                    <!-- Email input cambiar luego type a email-->
                     <div class="form-outline mb-3">
-                      <input type="email" id="Email" class="css-input"
-                        placeholder="Email" />
+                      <input type="text" id="nombre" class="css-input btn-block" name="nombre" required placeholder="Correo Institucional" />
                     </div>
           
                     <!-- Password input -->
                     <div class="form-outline mb-3">
-                      <input type="password" id="Password" class="css-input"
-                        placeholder="Password" />
-                    </div>
+                    <input type="password" name="password" id="password" class="css-input btn-block" required placeholder="Contraseña">
+                                <input type="hidden" name="accion" value="acceso_user">                    
+                            </div>
           
                     <div class="d-flex justify-content-between align-items-center">
                       <!-- Checkbox -->
                       <div class="form-check mb-0">
-                        <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                        <label class="form-check-label" for="form2Example3">
-                          Remember me
-                        </label>
+                        
                       </div>
-                      <a href="mailto:nahomi.nunez@isfodosu.edu.do?subject=Forgot%20Password"> Forgot password?</a>
+                      <a href="mailto:nahomi.nunez@isfodosu.edu.do?subject=Contraseña%20Olvidada" style="color: #174379"> Olvidé la contraseña</a>
                     </div>
+                    <br>
+                    <div class="d-flex justify-content-between align-items-center">
           
-                    <div class="text-center text-lg-start mt-4 pt-2">
-                        <a href="Formularios.html">
-                            <button type="button" class="ColorB"
-                            style="padding-left: 2.5rem; padding-right: 2.5rem; "> Login </button>
-                        </a>
+                        <input type="submit"class="ColorB btn-block" style="padding-left: 2.5rem; padding-right: 2.5rem;  " value="Login"> 
                     </div>
+                    
           
                   </form>
                 </div>
@@ -79,4 +70,7 @@
             </div>
 
     </body>
+
+
+
 </html>
