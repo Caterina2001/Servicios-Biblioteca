c82275d14ef807e061dbd8fef40f2efd6cc3e3e1
Nahomi Núñez <sotonahomi@gmail.com>
2023-06-12 15:52:41 -0400

    Vista del recinto LNNM completa - lista

--- Views/LNNM/index.php
+++ Views/LNNM/index.php
@@ -15,162 +15,129 @@ if( $validar == null || $validar = ''){
 ?>
 <!DOCTYPE html>
 <html lang="es-MX">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registros</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/es.css">
+        <link rel="stylesheet" href="./css/styles.css">
+        <link rel="stylesheet" href=".../css/estilo.css">
+        <link rel="stylesheet" href="./package/dist/sweetalert2.css">
+        <link rel="stylesheet" type="text/css" href="../../css/login.css">
+        <title>LNNM</title>
+    </head>
 
-	<link rel="stylesheet" href="./css/es.css">
-    <link rel="stylesheet" href="./css/styles.css">
-    <link rel="stylesheet" href=".../css/estilo.css">
-    <link rel="stylesheet" href="./package/dist/sweetalert2.css">
-    <link rel="stylesheet" type="text/css" href="../../css/login.css">
-</head>
+    <body>
 
-<body id="page-top">
-
-<div class="modal fade" id="createlnnm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h3 class="modal-title" id="exampleModalLabel">Registro de Participantes</h3>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form  action="" method="POST">
-                    <div class="form-group">
-                        <select class="css-input btn-block" style= " display: block; width: 100%;"  name="recinto" required id="recinto"  > 
-                        <option value="LNNM"  selected >LNNM</option>
-                        </select>
+        <div class="modal fade" id="createlnnm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 class="modal-title" id="exampleModalLabel">Registro de Participantes</h3>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
-                    <br>
+                    <div class="modal-body">
+                        <form  action="" method="POST">
+                            <div class="form-group">
+                                <select class="css-input btn-block" style= " display: block; width: 100%;"  name="recinto" required id="recinto"  > 
+                                    <option value="LNNM"  selected >LNNM</option>
+                                </select>
+                            </div>
+                            <br>
+                            <div class="form-group">
+                                <input type="text" id="nombre" name="nombre" class="css-input btn-block" style= " display: block; width: 100%;" required placeholder="Nombre">
+                            </div>
+                            <br>
+                            <div class="row form-group ">
+                                <div class="col-xs-6 col-sm-6 col-md-6">
+                                    <select class="css-input btn-block" style= " display: block; width: 100%;" required name="rol" id="rol"> 
+                                        <option value="" hidden selected >Tipo de Usuario</option>
+                                        <option value="Estudiante">Estudiante</option>
+                                        <option value="Docente">Docente</option>
+                                        <option value="Administrativo">Administrativo</option>
+                                        <option value="Externo">Externo</option>
+                                    </select>
+                                </div>
+                                <div class="col-xs-6 col-sm-6 col-md-6 ">
+                                    <input type="text" pattern="\d{9}|\d{11}" id="matricula" name="matricula btn-block" class="css-input" style= " display: block; width: 100%;" required placeholder="Matricula o Cedula">
+                                </div>
+                            </div>
+                            <br>
                     
-                    <div class="form-group">
-                        <input type="text" id="nombre" name="nombre" class="css-input btn-block" style= " display: block; width: 100%;" required placeholder="Nombre">
-                    </div>
-                   <!--  <br>
-                    <div class="form-group">
-                        <select class="css-input btn-block" style= " display: block; width: 100%;" required name="rol" id="rol"> 
-                        <option value="" hidden selected >Tipo de Usuario</option>
-                            <option value="Estudiante">Estudiante</option>
-                            <option value="Docente">Docente</option>
-                            <option value="Administrativo">Administrativo</option>
-                            <option value="Externo">Externo</option>
-                        </select>
-                    </div>
-                    <br>
-                    <div class="form-group">
-                        <input type="text" pattern="\d{9}|\d{11}" id="matricula" name="matricula btn-block" class="css-input" style= " display: block; width: 100%;" required placeholder="Matricula o Cedula">
-                    </div> -->
-                    <br>
-                    <div class="row form-group ">
-                        <div class="col-xs-6 col-sm-6 col-md-6">
-                        <select class="css-input btn-block" style= " display: block; width: 100%;" required name="rol" id="rol"> 
-                        <option value="" hidden selected >Tipo de Usuario</option>
-                            <option value="Estudiante">Estudiante</option>
-                            <option value="Docente">Docente</option>
-                            <option value="Administrativo">Administrativo</option>
-                            <option value="Externo">Externo</option>
-                        </select>
-                        </div>
-                        <div class="col-xs-6 col-sm-6 col-md-6 ">
-                        <input type="text" pattern="\d{9}|\d{11}" id="matricula" name="matricula btn-block" class="css-input" style= " display: block; width: 100%;" required placeholder="Matricula o Cedula">
-                        </div>
-                    </div>
-                    <br>
-                    
-                    <div class="form-group">
-                        <select class="css-input btn-block" style= " display: block; width: 100%;" required name="servicio" id="servicio" > 
-                        <option value="" hidden selected >Servicio</option>
-                            <option value="Sala de Estudio">Sala de Estudio</option>
-                            <option value="Sala de Lectura">Sala de Lectura</option>
-                            <option value="Computadoras">Computadoras</option>
-                            <option value="Fotocopiadoras">Fotocopiadoras</option>
-                        </select>
-                    </div>
-                    <br>
-                    <div class="form-group">
-                        <select class="css-input btn-block" style= " display: block; width: 100%;"  name="responsable" required id="responsable"  > 
-                        <option value="<?php echo $_SESSION['correo']; ?>"  selected ><?php echo $_SESSION['correo']; ?></option>
-                        </select>
-                    </div>
-                    <br>
-                    <div class="mb-3">       
-                        <input type="submit" value="Guardar" id="register" class="ColorB btn-block" style="padding-left: 2.5rem; padding-right: 2.5rem;" 
-                        name="registrar">
-                    </div>
+                            <div class="form-group">
+                                <select class="css-input btn-block" style= " display: block; width: 100%;" required name="servicio" id="servicio" > 
+                                    <option value="" hidden selected >Servicio</option>
+                                    <option value="Sala de Estudio">Sala de Estudio</option>
+                                    <option value="Sala de Lectura">Sala de Lectura</option>
+                                    <option value="Computadoras">Computadoras</option>
+                                    <option value="Fotocopiadoras">Fotocopiadoras</option>
+                                </select>
+                            </div>
+                            <br>
+                            <div class="form-group">
+                                <select class="css-input btn-block" style= " display: block; width: 100%;"  name="responsable" required id="responsable"  > 
+                                    <option value="<?php echo $_SESSION['correo']; ?>"  selected ><?php echo $_SESSION['correo']; ?></option>
+                                </select>
+                            </div>
+                            <br>
+                            <div class="mb-3">       
+                                <input type="submit" value="Guardar" id="register" class="ColorB btn-block" style="padding-left: 2.5rem; padding-right: 2.5rem;" name="registrar">
+                            </div>
 
-                </form>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
 
+        <script src="./package/dist/sweetalert2.all.js"></script>
+        <script src="./package/dist/sweetalert2.all.min.js"></script>
+
+        <script type="text/javascript">
+            $(function(){
+                $('#register').click(function(e){
+                    var valid = this.form.checkValidity();
+                    if(valid){
+                        var recinto = $('#recinto').val();
+                        var nombre = $('#nombre').val();
+                        var rol = $('#rol').val();
+                        var matricula = $('#matricula').val();
+                        var servicio = $('#servicio').val();
+                        var responsable = $('#responsable').val();
+                        e.preventDefault();	
+                        $.ajax({
+                            type: 'POST',
+                            url: '../../includes/validar.php',
+                            data: {recinto: recinto, nombre: nombre, rol: rol, matricula: matricula, servicio: servicio, responsable: responsable},
+                            success: function(data){
+                                Swal.fire({
+                                    'title': '¡Mensaje!',
+                                    'text': data,
+                                    'icon': 'success',
+                                    'showConfirmButton': 'false',
+                                    'timer': '1500'
+                                }).then(function() {
+                                    window.location = "registros.php";
+                                });    
+                            } ,       
+                            error: function(data){
+                                Swal.fire({
+                                    'title': 'Error',
+                                    'text': data,
+                                    'icon': 'error'
+                                })
+                            }
+                        });
 
                         
+                    }else{
+                        
+                    }
 
-<script src="./package/dist/sweetalert2.all.js"></script>
-<script src="./package/dist/sweetalert2.all.min.js"></script>
-
-<script type="text/javascript">
-	$(function(){
-		$('#register').click(function(e){
-
-			var valid = this.form.checkValidity();
-
-			if(valid){
-
-
-			var recinto = $('#recinto').val();
-			var nombre = $('#nombre').val();
-			var rol = $('#rol').val();
-			var matricula = $('#matricula').val();
-            var servicio = $('#servicio').val();
-            var responsable = $('#responsable').val();
-			
-
-				e.preventDefault();	
-
-				$.ajax({
-					type: 'POST',
-					url: '../../includes/validar.php',
-					data: {recinto: recinto, nombre: nombre, rol: rol, matricula: matricula, servicio: servicio, responsable: responsable},
-					success: function(data){
-					Swal.fire({
-								'title': '¡Mensaje!',
-								'text': data,
-                                'icon': 'success',
-                                'showConfirmButton': 'false',
-                                'timer': '1500'
-								}).then(function() {
-                window.location = "registros.php";
+                });		
             });
-							
-					} ,
-                    
-					error: function(data){
-						Swal.fire({
-								'title': 'Error',
-								'text': data,
-								'icon': 'error'
-								})
-					}
-				});
-
-				
-			}else{
-				
-			}
-
-		});		
-
-		
-	});
-    
-	
-</script>
-</body>
+        </script>
+    </body>
 </html>
